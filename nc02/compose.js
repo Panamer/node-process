c2d797432de9bcd30780fd77f588a436ec850bac
guoyongwei <guoyongwei@gomefinance.com.cn>
2020-07-07 10:43:46 +0800

    实现compose

--- nc02/compose.js
+++ nc02/compose.js
@@ -1,25 +1,33 @@
+// https://segmentfault.com/a/1190000016707187#item-7-5
+
+
 function compose(middlewares) {
-    return function() {
-      const first = middlewares[0]
-      console.log(first());
-      
-      middlewares.reduce((a, b) => {
-        console.log(a)
-        if (!a) {
-          return Promise.resolve()
+  return function () {
+    // 自执行 async 函数返回 Promise
+    return (async function () {
+      // 定义默认的 next，最后一个中间件内执行的 next
+      let next = async () => Promise.resolve();
+
+      // middleware 为每一个中间件函数，oldNext 为每个中间件函数中的 next
+      // 函数返回一个 async 作为新的 next，async 执行返回 Promise，解决异步问题
+      function createNext(middleware, oldNext) {
+        return async () => {
+          await middleware(oldNext);
         }
-        console.log(a.then())
-        return a.then(Promise.resolve(b()))
-       
-      }, first())
-    }
+      }
+
+      // 反向遍历中间件数组，先把 next 传给最后一个中间件函数
+      // 将新的中间件函数存入 next 变量
+      // 调用下一个中间件函数，将新生成的 next 传入
+      for (let i = middlewares.length - 1; i >= 0; i--) {
+        next = createNext(middlewares[i], next);
+      }
+
+      await next();
+    })();
+  }
 }
 
-console.log(
-  [1, 2, 3].reduce((a, b) => {
-    return a + b
-  })
-)
 // function compose(middlewares) {
 //   // middlewares 是数组  返回一个function
 //   return function () {
